01029b0505168fc7361375d2169ec4b5be71b9a7
Steka <stephenkaruru05@gmail.com>
2024-12-07 21:51:22 +0300

    OOP done

---
+++ weekFive/Class.py
@@ -0,0 +1,22 @@
+""" Assignment 1: Design Your Own Class! 🏗️
+Create a class representing anything you like (a Smartphone, Book, or even a Superhero!).
+Add attributes and methods to bring the class to life!
+Use constructors to initialize each object with unique values.
+Add an inheritance layer to explore polymorphism or encapsulation. """
+
+class Home:
+    def __init__(self,color,doors,rooms):
+        self.color = color
+        self.doors = doors
+        self.rooms = rooms 
+    
+
+class House(Home):
+    pass
+
+home2 = House("Blue","3","6")
+home1 = Home("green","4","2")
+
+
+print(home2.color)
+print(home1.color)
---
+++ weekFive/polymorphism.py
@@ -0,0 +1,13 @@
+""" Create a program that includes animals or vehicles with the same action (like move()). However, make each class define move() differently (for example, Car.move() prints "Driving" , while Plane.move() prints "Flying" ). """
+
+class Vehicle:
+    class Boat:
+        def move(self):
+            return "Floats!" 
+    class Car:
+        def move(self):
+            return "Driving!"
+
+    for vehicle in [Boat(),Car()]:
+        print(vehicle.move())
+   
